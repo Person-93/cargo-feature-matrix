b2b1f70967e2336e56e2098e6a6b54d58e24a7b8
person93 <person93.person93@gmail.com>
2021-12-01 12:15:40 -0500

    perf: Feature struct to wraps &str instead of String

--- src/config.rs
+++ src/config.rs
@@ -8,13 +8,14 @@ use std::collections::HashSet;
 
 #[derive(Default, Serialize, Deserialize)]
 #[non_exhaustive]
-pub struct Config {
+#[serde(bound(deserialize = "'de: 'c"))]
+pub struct Config<'c> {
     /// If this set is not empty, only these features will be used to construct the
     /// matrix.
-    pub seed: FeatureSet,
+    pub seed: FeatureSet<'c>,
 
     /// All of these features will be included in every feature set in the matrix.
-    pub include: FeatureSet,
+    pub include: FeatureSet<'c>,
 
     /// Any feature set that includes any of these will be excluded from the matrix.
     /// This includes features enabled by other features.
@@ -22,10 +23,10 @@ pub struct Config {
     /// This can be used for things like having an "__unstable" feature that gets
     /// enabled by any other features that use unstable rust features and then
     /// excluding "__unstable" if not on nightly.
-    pub deny: FeatureSet,
+    pub deny: FeatureSet<'c>,
 
     /// These sets will be dropped from the matrix.
-    pub skip: FeatureMatrix,
+    pub skip: FeatureMatrix<'c>,
 
     /// Some crates prepend internal features with a double underscore. If this
     /// flag is not set, those features will not be used to build the matrix, but
@@ -34,11 +35,11 @@ pub struct Config {
 
     /// List sets of features that can't be used together. Any generated feature
     /// set that is a superset of any of these sets will be dropped from the matrix.
-    pub conflict: HashSet<FeatureSet>,
+    pub conflict: HashSet<FeatureSet<'c>>,
 }
 
-impl Config {
-    pub fn from<T: Provider>(provider: T) -> Result<Config, Error> {
+impl Config<'_> {
+    pub fn from<T: Provider>(provider: T) -> Result<Self, Error> {
         Figment::from(provider).extract()
     }
 
@@ -47,7 +48,7 @@ impl Config {
     }
 }
 
-impl Provider for Config {
+impl Provider for Config<'_> {
     fn metadata(&self) -> Metadata {
         Metadata::named("Config object")
     }
--- src/features.rs
+++ src/features.rs
@@ -2,20 +2,19 @@ use derive_more::{AsMut, AsRef, Deref, DerefMut};
 use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 use std::{
     collections::{BTreeSet, HashSet},
-    convert::Infallible,
     fmt::{Display, Formatter},
-    str::FromStr,
+    ops::Deref,
 };
 
 #[derive(
     Clone, Debug, Default, Deref, DerefMut, AsRef, AsMut, Serialize, Deserialize,
 )]
-pub struct FeatureMatrix(HashSet<FeatureSet>);
+pub struct FeatureMatrix<'f>(#[serde(borrow)] HashSet<FeatureSet<'f>>);
 
 #[derive(
     Clone, Debug, Default, Eq, PartialEq, Hash, Deref, DerefMut, AsRef, AsMut,
 )]
-pub struct FeatureSet(BTreeSet<Feature>);
+pub struct FeatureSet<'f>(BTreeSet<Feature<'f>>);
 
 #[derive(
     Clone,
@@ -32,12 +31,10 @@ pub struct FeatureSet(BTreeSet<Feature>);
     Serialize,
     Deserialize,
 )]
-#[as_ref(forward)]
-#[as_mut(forward)]
 #[serde(transparent)]
-pub struct Feature(String);
+pub struct Feature<'f>(pub(crate) &'f str);
 
-impl Serialize for FeatureSet {
+impl Serialize for FeatureSet<'_> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
@@ -46,7 +43,7 @@ impl Serialize for FeatureSet {
     }
 }
 
-impl<'de> Deserialize<'de> for FeatureSet {
+impl<'de: 'f, 'f> Deserialize<'de> for FeatureSet<'f> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
@@ -56,23 +53,26 @@ impl<'de> Deserialize<'de> for FeatureSet {
         struct Visitor;
 
         impl<'de> de::Visitor<'de> for Visitor {
-            type Value = FeatureSet;
+            type Value = FeatureSet<'de>;
 
             fn expecting(&self, f: &mut Formatter) -> std::fmt::Result {
                 write!(f, "a valid feature set")
             }
 
-            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            fn visit_borrowed_str<E>(
+                self,
+                v: &'de str,
+            ) -> Result<Self::Value, E>
             where
                 E: de::Error,
             {
-                Ok(v.parse().unwrap())
+                Ok(FeatureSet::from(v))
             }
         }
     }
 }
 
-impl Display for FeatureSet {
+impl Display for FeatureSet<'_> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let mut iter = self.iter();
         if let Some(feature) = iter.next() {
@@ -85,21 +85,19 @@ impl Display for FeatureSet {
     }
 }
 
-impl FromStr for FeatureSet {
-    type Err = Infallible;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(s.split(',').map(ToString::to_string).map(Feature).collect())
+impl<'f> From<&'f str> for FeatureSet<'f> {
+    fn from(s: &'f str) -> Self {
+        s.split(',').map(Feature).collect()
     }
 }
 
-impl FromIterator<Feature> for FeatureSet {
-    fn from_iter<T: IntoIterator<Item = Feature>>(iter: T) -> Self {
+impl<'f> FromIterator<Feature<'f>> for FeatureSet<'f> {
+    fn from_iter<T: IntoIterator<Item = Feature<'f>>>(iter: T) -> Self {
         FeatureSet(iter.into_iter().collect())
     }
 }
 
-impl Display for Feature {
+impl Display for Feature<'_> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         Display::fmt(&self.0, f)
     }
