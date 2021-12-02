cca18765f882bca94fee362e0f99d22987957f64
person93 <person93.person93@gmail.com>
2021-12-01 20:07:37 -0500

    feat: implement run function - the entrypoint into the library

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,55 @@ mod execution;
 pub mod features;
 
 pub use self::{config::Config, execution::TaskKind};
+use self::{execution::Task, features::FeatureMatrix};
+use cargo_metadata::{Metadata, MetadataCommand, Package};
+use itertools::Itertools;
+use std::path::PathBuf;
 use thiserror::Error;
+
+pub fn run(
+    command: String,
+    args: Vec<String>,
+    task: TaskKind,
+    manifest_path: Option<PathBuf>,
+    config: Config,
+) -> Result<(), Error> {
+    let mut cmd = MetadataCommand::new();
+    if let Some(manifest_path) = manifest_path {
+        cmd.manifest_path(manifest_path);
+    }
+    let metadata = cmd.exec()?;
+
+    let matrices: Vec<_> = get_workspace_members(&metadata)
+        .map(|package| {
+            FeatureMatrix::new(package, &config)
+                .map(|matrix| (&package.name, matrix))
+        })
+        .try_collect()?;
+
+    let (_, errors): (Vec<_>, Vec<_>) = matrices
+        .into_iter()
+        .map(|(package_name, matrix)| {
+            Task::new(task, &command, package_name, &args, matrix)
+        })
+        .map(Task::run)
+        .partition_result();
+
+    match errors.into_iter().next() {
+        Some(err) => Err(err),
+        None => Ok(()),
+    }
+}
+
+/// Gets a list of packages that are members of the workspace
+fn get_workspace_members(
+    metadata: &Metadata,
+) -> impl Iterator<Item = &Package> + '_ {
+    metadata
+        .packages
+        .iter()
+        .filter(|package| metadata.workspace_members.contains(&package.id))
+}
 
 #[derive(Debug, Error)]
 pub enum Error {
