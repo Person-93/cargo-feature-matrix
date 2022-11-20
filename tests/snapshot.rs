01e97b88a1c04b622160447df1f25fec651334b2
Matthew Dharm <85530043+mdharm@users.noreply.github.com>
2022-11-19 18:27:16 -0800

    fix: Use --no-default-features
    If --no-default-features is NOT used, the the selected features used in
    a given build are those selected by cargo-feature-matrix PLUS the
    default features.   This is contrary to the UI output and likely
    contrary to the desired behavior of a test matrix.
    
    This commit adds --no-default-features to the command arguments.  A new
    test case is also added which will fail if the default features are not
    properly supressed.
    
    Along the way, the `features` sample was updated to use a new version of
    `rand`, as the version specified doesn't actually compile with a current
    stable compiler.

--- tests/snapshot.rs
+++ tests/snapshot.rs
@@ -30,6 +30,32 @@ fn test() {
     });
 }
 
+#[test]
+fn test_no_default_features() {
+    glob!("samples/seed/Cargo.toml", |path| {
+        let path = path
+            .parent()
+            .expect("failed to unwrap parent of Cargo.toml");
+
+        let mut cmd = Command::new(BIN.as_os_str());
+        let assert = cmd
+            .arg("feature-matrix")
+            .arg("--color=never")
+            .arg("test")
+            .current_dir(path)
+            .assert()
+            .success();
+        let output = assert.get_output();
+        let output = std::str::from_utf8(&output.stdout)
+            .expect("child process's output included invalid unicode");
+
+        with_settings!({snapshot_suffix => path.file_name().unwrap().to_str().unwrap()},
+        {
+            assert_snapshot!(output);
+        });
+    });
+}
+
 lazy_static! {
     static ref BIN: PathBuf =
         assert_cmd::cargo::cargo_bin("cargo-feature-matrix");
